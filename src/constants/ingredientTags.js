bb32e4ba83085dcaf648a9c4fbdc1fc200be207e
Iurii <iamyurkas@gmail.com>
2025-08-13 22:25:05 +0100

    Changed ingredients tags colors

--- src/constants/ingredientTags.js
+++ src/constants/ingredientTags.js
@@ -1,13 +1,13 @@
 export const BUILTIN_INGREDIENT_TAGS = [
   { id: 1, name: "strong alcohol", color: "#FF6B6B" },
-  { id: 2, name: "soft alcohol", color: "#FF8787" },
-  { id: 3, name: "beverage", color: "#8AADCFFF" },
-  { id: 4, name: "syrup", color: "#FFD43B" },
-  { id: 5, name: "juice", color: "#FFA94D" },
+  { id: 2, name: "soft alcohol", color: "#FFA94D" },
+  { id: 3, name: "beverage", color: "#FFD43B" },
+  { id: 4, name: "syrup", color: "#8AADCFFF" },
+  { id: 5, name: "juice", color: "#c6daffff" },
   { id: 6, name: "fruit", color: "#69DB7C" },
   { id: 7, name: "herb", color: "#38D9A9" },
-  { id: 8, name: "spice", color: "#9775FA" },
-  { id: 9, name: "dairy", color: "#4DABF7" },
+  { id: 8, name: "spice", color: "#000000ff" },
+  { id: 9, name: "dairy", color: "#757575ff" },
   { id: 10, name: "other", color: "#AFC9C3FF" },
 ];
 
