1cfc670f938e2fc327e820eb6af604f75144f166
Iurii <iamyurkas@gmail.com>
2025-08-13 13:16:27 +0100

    feat: remember last cocktails tab

--- src/screens/Cocktails/MyCocktailsScreen.js
+++ src/screens/Cocktails/MyCocktailsScreen.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { useTabMemory } from "../../context/TabMemoryContext";
 
 export default function MyCocktailsScreen() {
+  const { setTab } = useTabMemory();
+  const didSetTabRef = useRef(false);
+
+  useEffect(() => {
+    if (!didSetTabRef.current) {
+      setTab("cocktails", "My");
+      didSetTabRef.current = true;
+    }
+  }, [setTab]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>My cocktails coming soon!</Text>
