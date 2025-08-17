17b7832ef27eb62b1d8551b7adc6946f0af3ca49
Iurii <iamyurkas@gmail.com>
2025-08-17 18:05:19 +0100

    Fixed tags colors

--- src/constants/ingredientTags.js
+++ src/constants/ingredientTags.js
@@ -3,13 +3,13 @@ import { TAG_COLORS } from "../theme";
 export const BUILTIN_INGREDIENT_TAGS = [
   { id: 1, name: "strong alcohol", color: TAG_COLORS[0] },
   { id: 2, name: "soft alcohol", color: TAG_COLORS[1] },
-  { id: 3, name: "beverage", color: TAG_COLORS[2] },
-  { id: 4, name: "syrup", color: TAG_COLORS[3] },
-  { id: 5, name: "juice", color: TAG_COLORS[4] },
-  { id: 6, name: "fruit", color: TAG_COLORS[5] },
-  { id: 7, name: "herb", color: TAG_COLORS[6] },
-  { id: 8, name: "spice", color: TAG_COLORS[7] },
-  { id: 9, name: "dairy", color: TAG_COLORS[8] },
+  { id: 3, name: "beverage", color: TAG_COLORS[3] },
+  { id: 4, name: "syrup", color: TAG_COLORS[13] },
+  { id: 5, name: "juice", color: TAG_COLORS[10] },
+  { id: 6, name: "fruit", color: TAG_COLORS[9] },
+  { id: 7, name: "herb", color: TAG_COLORS[8] },
+  { id: 8, name: "spice", color: TAG_COLORS[14] },
+  { id: 9, name: "dairy", color: TAG_COLORS[6] },
   { id: 10, name: "other", color: TAG_COLORS[15] },
 ];
 
