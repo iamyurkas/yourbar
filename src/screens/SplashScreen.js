d0c3d431729f8842117959d7dd9e5a64607f860e
Iurii <iamyurkas@gmail.com>
2025-08-22 11:31:44 +0100

    Animate splash screen with tab icons

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,13 +1,35 @@
-import React from "react";
-import { View, Image, StyleSheet, Text } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { View, StyleSheet, Text, Animated } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import ShakerIcon from "../../assets/shaker.svg";
+import IngredientIcon from "../../assets/lemon.svg";
 
 export default function SplashScreen() {
+  const cocktailScale = useRef(new Animated.Value(0)).current;
+  const shakerScale = useRef(new Animated.Value(0)).current;
+  const ingredientScale = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.stagger(100, [
+      Animated.spring(cocktailScale, { toValue: 1, useNativeDriver: true }),
+      Animated.spring(shakerScale, { toValue: 1, useNativeDriver: true }),
+      Animated.spring(ingredientScale, { toValue: 1, useNativeDriver: true }),
+    ]).start();
+  }, [cocktailScale, shakerScale, ingredientScale]);
+
   return (
     <View style={styles.container}>
-      <Image
-        source={require("../../assets/splash-icon.png")}
-        style={styles.image}
-      />
+      <View style={styles.iconsRow}>
+        <Animated.View style={{ transform: [{ scale: cocktailScale }] }}>
+          <MaterialIcons name="local-bar" size={64} color="#333" />
+        </Animated.View>
+        <Animated.View style={{ transform: [{ scale: shakerScale }] }}>
+          <ShakerIcon width={64} height={64} fill="#333" />
+        </Animated.View>
+        <Animated.View style={{ transform: [{ scale: ingredientScale }] }}>
+          <IngredientIcon width={64} height={64} fill="#333" />
+        </Animated.View>
+      </View>
       <Text style={styles.title}>Your bar</Text>
       <Text style={styles.tagline}>your rules</Text>
     </View>
@@ -21,11 +43,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#ffffff",
   },
-  image: {
-    width: 200,
-    height: 200,
+  iconsRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: 240,
     marginBottom: 20,
-    resizeMode: "contain",
   },
   title: {
     fontSize: 32,
