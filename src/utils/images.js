27fd53e9b0f0f8b032ab8709571a587c11469b61
Iurii <iamyurkas@gmail.com>
2025-09-02 16:20:03 +0100

    refactor: use new image manipulator API

--- src/utils/images.js
+++ src/utils/images.js
@@ -2,20 +2,19 @@ import * as ImageManipulator from 'expo-image-manipulator';
 
 export async function resizeImage(uri, maxSize = 150) {
   try {
-    const info = await ImageManipulator.manipulateAsync(uri, []);
-    const resizeAction =
-      info.width > info.height
-        ? { resize: { width: maxSize } }
-        : { resize: { height: maxSize } };
-    const actions = [
-      { extent: { width: info.width, height: info.height, backgroundColor: '#fff' } },
-      resizeAction,
-    ];
-    const result = await ImageManipulator.manipulateAsync(
-      uri,
-      actions,
-      { compress: 0.7, format: ImageManipulator.SaveFormat.JPEG }
-    );
+    const { width, height } = await ImageManipulator.manipulate(uri).renderAsync();
+    const context = ImageManipulator.manipulate(uri);
+    context.extent({ width, height, backgroundColor: '#fff' });
+    if (width > height) {
+      context.resize({ width: maxSize });
+    } else {
+      context.resize({ height: maxSize });
+    }
+    const rendered = await context.renderAsync();
+    const result = await rendered.saveAsync({
+      compress: 0.7,
+      format: ImageManipulator.SaveFormat.JPEG,
+    });
     return result.uri;
   } catch (e) {
     console.warn('resizeImage failed', e);
