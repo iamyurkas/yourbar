03ba312a770957ecc64d9de524046acbff84d97f
Iurii <iamyurkas@gmail.com>
2025-09-10 12:54:40 +0100

    feat: log all sql queries

--- src/data/__mocks__/expo-sqlite.ts
+++ src/data/__mocks__/expo-sqlite.ts
@@ -1,12 +1,23 @@
+let tracer;
+
 export function openDatabaseSync() {
   return {
-    execAsync: async () => {},
-    getAllAsync: async () => [],
-    runAsync: async () => {},
+    execAsync: async (sql, params) => {
+      tracer?.(sql, params);
+    },
+    getAllAsync: async (sql, params) => {
+      tracer?.(sql, params);
+      return [];
+    },
+    runAsync: async (sql, params) => {
+      tracer?.(sql, params);
+    },
     withTransactionAsync: async (cb) => cb(),
   };
 }
-export function setTracer() {}
+export function setTracer(fn) {
+  tracer = fn;
+}
 export function withTransactionAsync(_db, cb) {
   return cb();
 }
